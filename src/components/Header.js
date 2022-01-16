28567aa5f5d5c0ae96e48bd58ab50fdd5d3f4280
Takashi <th2826@columbia.edu>
2022-01-16 10:08:28 -0500

    fix: logo color becomes white when background is black

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavBar from "./NavBar";
+import { BackgroundContext } from './context/background';
 
 function Header(){
+  const { background } = useContext(BackgroundContext);
+
+  const style = {color: background !== "black" ? "black" : "white"};
+  
   return (
     <header>
-      <h1 id="logo">TH</h1>
+      <h1 id="logo" style={style}>TH</h1>
       <NavBar />
     </header>
   )
