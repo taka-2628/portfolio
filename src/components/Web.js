6bd3bd73b4b1202b07b8413f7d165733bc077399
Takashi <th2826@columbia.edu>
2022-01-17 22:56:21 -0500

    fix: added subtitle & fixed links

--- src/components/Web.js
+++ src/components/Web.js
@@ -4,7 +4,8 @@ import { BackgroundContext } from './context/background';
 
 import bundleGIF from "../assets/01-BUNDLE.gif";
 import actorGIF from "../assets/02-52711-Actors.gif";
-import mediumLogo from "../assets/medium-logo_white.png"
+import mediumLogo from "../assets/medium-logo_white.png";
+import githubLogo from "../assets/github-logo_white.png";
 
 function Web(){
   const match = useRouteMatch();
@@ -24,16 +25,19 @@ function Web(){
         </a>
         <div className="work-description">
           <a href="https://taka-2628.github.io/BUNDLE/" target="_blank" rel="noopener noreferrer" className="web-project-link">
-            <h2>BUNDLE</h2>
+            <h2 className="title">BUNDLE</h2>
           </a>
+          <i className="subtitle">Single-page Application with Vanilla JavaScript and JSON-server on Heroku</i>
           <p className="project-detail">
-            Flatiron School | 2021 
-            <a href="https://takashi-honzawa.medium.com/bundle-my-first-spa-project-3e1a7f692b27" target="_blank" rel="noopener noreferrer">
-              <img className="website-logo" src={mediumLogo}></img>
-            </a>
-            <a href="https://takashi-honzawa.medium.com/bundle-my-first-spa-project-3e1a7f692b27" target="_blank" rel="noopener noreferrer">
-              <img className="website-logo" src={mediumLogo}></img>
-            </a>
+            Flatiron School | 2021 |
+            <span className="website-logo-wrapper">
+              <a href="https://takashi-honzawa.medium.com/bundle-my-first-spa-project-3e1a7f692b27" target="_blank" rel="noopener noreferrer">
+                <img className="website-logo" src={mediumLogo}></img>
+              </a>
+              <a href="https://github.com/taka-2628/portfolio" target="_blank" rel="noopener noreferrer">
+                <img className="website-logo" src={githubLogo}></img>
+              </a>
+            </span>
           </p>
           <p className="paragraph">
             BUNDLE is a simple portfolio aggregator/tracker for stock and crypto investors. It fetches real-time price data from APIs and calculates the total value and unrealized gain/loss of a portfolio.
@@ -47,20 +51,22 @@ function Web(){
 
         <div className="work-description">
           <a href="https://taka-2628.github.io/52711_ACTORS/" target="_blank" rel="noopener noreferrer" className="web-project-link">
-            <h2>LIFE AND DEATH OF 52,711 ACTORS</h2>
+            <h2 className="title">LIFE AND DEATH OF 52,711 ACTORS</h2>
           </a>
+          <i className="subtitle">Data Visualization with D3.js and Mapbox API</i>
           <p className="project-detail">
-            Columbia University | 2021
-            <a href="https://takashi-honzawa.medium.com/bundle-my-first-spa-project-3e1a7f692b27" target="_blank" rel="noopener noreferrer">
-              <img className="website-logo" src={mediumLogo}></img>
-            </a>
-            <a href="https://takashi-honzawa.medium.com/bundle-my-first-spa-project-3e1a7f692b27" target="_blank" rel="noopener noreferrer">
-              <img className="website-logo" src={mediumLogo}></img>
-            </a>
+            Columbia University | 2021 |
+            <span className="website-logo-wrapper">
+              <a href="https://github.com/taka-2628/52711_ACTORS" target="_blank" rel="noopener noreferrer">
+                <img className="website-logo" src={githubLogo}></img>
+              </a>
+            </span>
+            <span className="collaborator">- in collaboration with&nbsp; 
+              <a href="https://www.arch.columbia.edu/student-work?student_name_contains=Anays%20Gonzalez" target="_blank" rel="noopener noreferrer">Anays Gonzalez</a>
+            </span>
           </p>
           <p className="paragraph">
             Data Visualization project that explores the correlation between the main genre of an actor and his or her maridge, divorce and cause of death. Are romance actors happily married? Do actors of the same genre die of the same cause?
-
           </p>
         </div>
       </div>
