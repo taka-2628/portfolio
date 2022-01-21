a77cec8c5e87656c0545d592a25ce7359f11584c
Takashi <th2826@columbia.edu>
2022-01-20 22:53:33 -0500

    c-II project files added

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect }  from "react";
 import { useRouteMatch } from "react-router-dom";
 import { BackgroundContext } from './context/background';
 
+import noline from "../assets/space-design-tech/c-II-01.png";
+
 function Photography(){
   const match = useRouteMatch();
   console.log(match);
@@ -15,6 +17,7 @@ function Photography(){
   return (
     <div>
       <h1>Photography</h1>
+      <img src={noline} width="100%"></img>
     </div>
   )
 }
