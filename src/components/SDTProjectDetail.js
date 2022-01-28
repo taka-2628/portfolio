e7520b0966a1585331258cc6b4c227e3eff26a51
Takashi <th2826@columbia.edu>
2022-01-27 23:46:29 -0500

    feat: added SDTProjectDetail component

--- src/components/SDTProjectDetail.js
+++ src/components/SDTProjectDetail.js
@@ -4,10 +4,11 @@ import { useParams } from "react-router-dom";
 function SDTProject({ data }){
   const params = useParams();
   console.log(params);
-
+ 
   return (
     <div>
       <h1>hello</h1>
+      { data.filter(project => (project.id = params.id)).map(project => (<img key={project.title} src={project.cover}></img>)) }
     </div>
   )
 }
