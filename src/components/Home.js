36de98bc85f649389bc77a9dd44b7c2b2ece4812
Takashi <th2826@columbia.edu>
2022-02-09 21:37:58 -0500

    Home component updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { useRouteMatch } from "react-router-dom";
 import "../stylesheets/Home.css";
 
 import SelectedWork from "./SelectedWorks";
+import profileImage from "../assets/000062150015_rect2.jpg";
 
 function Home( { web, sdt } ){
   const match = useRouteMatch();
@@ -10,9 +11,19 @@ function Home( { web, sdt } ){
   
   return (
     <div id="home">
-      <h1 id="name">Takashi Honzawa</h1>
-      <p id="self-description">Architectural Designer turned Creative Developer/Product Designer.</p>
-      <p>I am now finishing my Master of Architecture degree at Columbia University and Software Engineering program at Flatiron School.</p>
+      <div id="introduction">
+        <div id="about-myself">
+          <h1 id="name">Takashi Honzawa</h1>
+          <p id="self-description"><strong>Architectural Designer</strong> turned <strong>Creative Developer/Product Designer</strong>.</p>
+          <div>
+            <p>I have experience in spatial, interactive, and visual designs and am skilled at front-end & back-end development. I am now finishing my Master of Architecture degree at Columbia University and Software Engineering program at Flatiron School.</p>
+          </div>
+        </div>
+        <div id="profile-image-wrapper">
+            <img src={profileImage}></img>
+        </div>
+      </div>
+
       <SelectedWork web={web} sdt={sdt} />
     </div>
   )
